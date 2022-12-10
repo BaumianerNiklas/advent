56716b157d4b1e71be96b07bb38522ed43b2cc7b
BaumianerNiklas <68434250+BaumianerNiklas@users.noreply.github.com>
2022-12-10 15:21:40 +0100

    feat: day 10

---
+++ 2022/08/main.py
@@ -0,0 +1,44 @@
+lines = []
+
+with open("./input.txt") as f:
+    lines = [l.strip() for l in f.read().split("\n")]
+
+X = 1
+cycles = 0
+strength = 0
+img = ["" for _ in range(6)]
+
+def update():
+    # wow such functional programming
+    global strength
+    global cycles
+    global img
+
+    if (cycles - 20) % 40 == 0:
+        strength += X * cycles
+    
+    if cycles // 40 > len(img) - 1:
+        return 
+    
+
+    img[cycles // 40] += "#" if cycles % 40 in range(X-1, X+2) else "."
+
+
+for line in lines:
+    match line.split(" "):
+        case ["noop"]:
+            update()
+            cycles += 1
+        
+        case ["addx", num]:
+            update()
+            cycles += 1
+            update()
+            cycles += 1
+
+            X += int(num)
+
+print(strength)
+
+for line in img:
+    print(*line)
